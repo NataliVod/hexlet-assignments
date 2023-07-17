681ec044b7682ad99959da434821567c7603664d
@hexlet/cli <support@hexlet.io>
2023-07-18 00:07:24 +0300

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/model/Person.java
@@ -0,0 +1,27 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Entity
+// END
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private long id;
+
+    // BEGIN
+    @Getter @Setter
+    private String firstName;
+
+    @Getter @Setter
+    private String lastName;
+    // END
+}
