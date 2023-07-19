33e06fefc00a58d058c21c9afa28330ce23a635c
@hexlet/cli <support@hexlet.io>
2023-07-19 04:50:21 +0300

    submit java-spring-ru/relations

---
+++ java-spring-ru/relations/src/main/java/exercise/dto/ArticleDto.java
@@ -0,0 +1,16 @@
+package exercise.dto;
+
+import exercise.model.Category;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Data
+public class ArticleDto {
+    private String name;
+    private String body;
+    private String category;
+}
+
+// END
