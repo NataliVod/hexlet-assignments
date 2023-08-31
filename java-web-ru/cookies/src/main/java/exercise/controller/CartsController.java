35fc6241d3b98dc0363c163b5fbfdef66c4955d7
@hexlet/cli <support@hexlet.io>
2023-08-31 21:26:28 +0300

    submit java-web-ru/cookies

---
+++ java-web-ru/cookies/src/main/java/exercise/controller/CartsController.java
@@ -0,0 +1,58 @@
+package exercise.controller;
+
+import java.util.Collections;
+import java.util.Map;
+import exercise.dto.CartPage;
+import exercise.util.NamedRoutes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import exercise.model.CartItem;
+import org.eclipse.jetty.util.UrlEncoded;
+
+import io.javalin.http.Context;
+
+
+public class CartsController {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static void index(Context ctx) throws Exception {
+        var cookie = ctx.cookie("cart") == null ? "{}" : ctx.cookie("cart");
+
+        Map<String, CartItem> cart = mapper.readValue(
+            UrlEncoded.decodeString(cookie),
+            new TypeReference<Map<String, CartItem>>() { }
+        );
+
+        var page = new CartPage(cart);
+        ctx.render("index.jte", Collections.singletonMap("page", page));
+    }
+
+    // BEGIN
+    public static void addItem(Context ctx) throws Exception {
+        var id = ctx.formParam("id");
+        var name = ctx.formParam("name");
+
+        var cookie = ctx.cookie("cart") == null ? "{}" : ctx.cookie("cart");
+
+        Map<String, CartItem> cart = mapper.readValue(
+                UrlEncoded.decodeString(cookie),
+                new TypeReference<Map<String, CartItem>>() { }
+        );
+
+        if (!cart.containsKey(id)) {
+            cart.put(id, new CartItem(name, 0));
+        }
+
+        cart.get(id).increaseCount();
+
+        var encodedCart = UrlEncoded.encodeString(mapper.writeValueAsString(cart));
+        ctx.cookie("cart", encodedCart);
+        ctx.redirect(NamedRoutes.rootPath());
+    }
+
+    public static void clearCart(Context ctx) throws Exception {
+        ctx.cookie("cart", "{}");
+        ctx.redirect(NamedRoutes.rootPath());
+    }
+    // END
+}
